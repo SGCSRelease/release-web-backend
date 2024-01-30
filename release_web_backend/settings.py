add6eab261e516d488164c91435714b05d425a18
hyunsoo <sweetonion135@gmail.com>
2024-01-30 13:26:06 +0000

    session token settings

--- release_web_backend/settings.py
+++ release_web_backend/settings.py
@@ -129,3 +129,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SESSION_COOKIE_HTTPONLY = True
+SESSION_COOKIE_SECURE = True
