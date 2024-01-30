711e39eb3b0908dca1588bc8ca5a7c6dbfaeefdc
hyunsoo <sweetonion135@gmail.com>
2024-01-30 13:25:45 +0000

    fixed tokens

--- utils/auth.py
+++ utils/auth.py
@@ -6,6 +6,7 @@ from release_web_backend import settings
 JWT_KEY = settings.get_secret('JWT_KEY')
 
 def get_member(id: int, password: str) -> Optional[Member]:
+
     members = Member.objects.filter(id=id)
     if members.count() == 0:
         return None
@@ -16,13 +17,26 @@ def get_member(id: int, password: str) -> Optional[Member]:
     else:
         return None
 
+
 def get_member_from_token(token: str) -> Optional[Member]:
-    payload = jwt.decode(token, JWT_KEY)
-    return get_member(payload['id'], payload['password'])
+
+    payload = jwt.decode(token, JWT_KEY, algorithms='HS256')
+    
+    members = Member.objects.filter(id=payload['id'])
+    if not members.exists():
+        return None
+    
+    member = members[0]
+    if member.password == payload['password'].encode('utf-8'):
+        return member
+    else:
+        return None
+
 
 def get_token(member: Member) -> str:
-    token = {'id': member.id, 'password': member.password}
-    return jwt.encode(token, JWT_KEY)
+    token = {'id': member.id, 'password': member.password.decode('utf-8')}
+    return jwt.encode(token, JWT_KEY, algorithm='HS256')
+
 
 def encrypt_password(password: str) -> str:
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
